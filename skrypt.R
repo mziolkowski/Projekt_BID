07186d741ffd065f0e70d52952d0c4b0213f5ec8
m.ziolkowski <ziolek0321@gmail.com>
2020-06-22 21:24:52 +0200

    Dodano Wykresy dla 3 krajów z całego okresu ich pandemii. Zaczęto robić predykcje.

--- skrypt.R
+++ skrypt.R
@@ -38,3 +38,47 @@ nowy.zestaw.covid
 
 regresja.Covid.All <- lm(deaths ~., data = Covid)
 regresja.Covid.All
+
+# Regresja liniowa dla 
+attach(COVID_19)
+regresja.cases <- lm(cases~day, data = COVID_19)
+summary(regresja.cases)
+
+# Wykresy dla Polski, Szwecji i Włoch
+COVID_19$countriesAndTerritories <- factor(COVID_19$countriesAndTerritories)
+COVID_19$geoId <- factor(COVID_19$geoId)
+COVID_19$countryterritoryCode <- factor(COVID_19$countryterritoryCode)
+COVID_19$continentExp <- factor(COVID_19$continentExp)
+COVID_19$dateRep <- factor(COVID_19$dateRep)
+
+
+?subset()
+dane <- (geoId="PL")
+
+
+danePL <- COVID_19[COVID_19$geoId=='PL',]
+danePL
+danePL$id
+daneSE <- COVID_19[COVID_19$geoId=='SE',]
+daneSE
+daneIT <- COVID_19[COVID_19$geoId=='IT',]
+daneIT
+
+?convert()
+plot(danePL$days, danePL$cases)
+reg.pl <- lm(danePL$cases~danePL$days)
+reg.se <- lm(daneSE$cases~daneSE$days)
+reg.it <- lm(daneIT$cases~daneIT$days)
+summary(reg.pl)
+summary(reg.se)
+summary(reg.it)
+#treningowy.pl <- danePL[1:(length(danePL)*0.8),]
+treningowy.pl <- danePL[1:80,]
+treningowy.pl.2 <- danePL[1:20,]
+treningowy.pl
+summary(reg.pl)
+abline(reg.pl$coefficients, col='red', lwd=2)
+?predict
+#predykcja.pl <- predict(reg.pl, treningowy.pl, typ = "response") # nie wiem czy jest OK bo mamy response a nie interval jak w innych zadaniach z regresji liniowej 
+predykcja.pl <- predict(reg.pl, treningowy.pl, interval = "prediction")
+predykcja.pl
